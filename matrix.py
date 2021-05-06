83e8dbf3ba7778d30d0951cacdd8bed0ff4f40b1
Ayarmz <johnayarmahe@gmail.com>
2021-05-06 21:37:51 +0000

    added a number or methods to matrix class

--- matrix.py
+++ matrix.py
@@ -1,59 +1,166 @@
+"""
+This class models a matrix
+"""
+
+from collections import namedtuple
+
+# Named tuple to describe dimension of matrix
+Dimension = namedtuple('Dimension', ['rows', 'cols'])
+
+
 class Matrix():
-    def __init__(self):
-        pass
+    def __init__(self, values: list):
+        self.values = values
+        self.dim = Dimension(len(values), len(values[0]))
 
     @classmethod
-    def unit_matrix(cls, n) -> Matrix:
+    def from_string(cls, s):
+        """
+        Create a matrix from string
+        """
+        values = [r.split() for r in s.split('; ')]
+        return cls(values)
+
+    @classmethod
+    def from_zeros(cls, r, c=None):
+        if c == None:
+            values = [[0]*r for _ in range(r)]
+            return cls(values)
+        else:
+            values = [[0]*c for _ in range(r)]
+            return cls(values)
+
+    @classmethod
+    def unit_matrix(cls, n):
         """Create an n by n unit matrix"""
-        pass
+        zeros_matrix = cls.from_zeros(n)
+        values = zeros_matrix.values
+        for i in range(n):
+            row = values[i]
+            row[i] = 1
+        return zeros_matrix
 
-    @classmethod
-    def utm(cls, m) -> Matrix:
+    @staticmethod
+    def utm_from(m):
         """Create an upper triangular matrix from matrix m"""
-        pass
+        rows, cols = m.dim
+        if rows == cols:
+            for i in range(len(m.values)):
+                el = m.values[i]
+                for j in range(len(el)):
+                    if j < i:
+                        m.values[i][j] = 0
+        else:
+            raise ValueError('Dimension error')
+        return m
 
-    @classmethod
-    def unit_utm(cls, m) -> Matrix:
+    @staticmethod
+    def unit_utm_from(m):
         """Create a unit upper triangular matrix from matrix m"""
-        pass
+        rows, cols = m.dim
+        if rows == cols:
+            for i in range(len(m.values)):
+                el = m.values[i]
+                for j in range(len(el)):
+                    if j < i:
+                        m.values[i][j] = 0
+                    elif j == i:
+                        m.values[i][j] = 1
+        else:
+            raise ValueError('Dimension error')
+        return m
 
-    @classmethod
-    def ltm(cls, m) -> Matrix:
+    @staticmethod
+    def ltm_from(m):
         """Create a lower triangular matrix from matrix m"""
+        rows, cols = m.dim
+        if rows == cols:
+            for i in range(len(m.values)):
+                el = m.values[i]
+                for j in range(len(el)):
+                    if j > i:
+                        m.values[i][j] = 0
+        else:
+            raise ValueError('Dimension error')
+        return m
+
+    @staticmethod
+    def unit_ltm_from(m):
+        """Create a unit lower triangular matrix from matrix m"""
+        rows, cols = m.dim
+        if rows == cols:
+            for i in range(len(m.values)):
+                el = m.values[i]
+                for j in range(len(el)):
+                    if j > i:
+                        m.values[i][j] = 0
+                    elif j == i:
+                        m.values[i][j] = 1
+        else:
+            raise ValueError('Dimension error')
+        return m
+
+    def trns(self):
+        """return the transpose of a matrix"""
+        cols, rows = self.dim
+        t_m = self.from_zeros(rows, cols)
+        values = (self.values[j][i] for i, v in enumerate(t_m.values)
+                  for j, _ in enumerate(v))
+        for k in range(rows):
+            for l in range(cols):
+                t_m.values[k][l] = next(values)
+        return t_m
+
+    def det(self):
         pass
 
-    @classmethod
-    def unit_ltm(cls, m) -> Matrix:
-        """Create a unit lower triangular matrix from matrix m"""
-        pass
-
-    @classmethod
-    def transpose(cls, m) -> Matrix:
-        """Create a unit lower triangular matrix from matrix m"""
+    def inv(self):
+        """Return the inverse of a matrix"""
         pass
 
     def __str__(self) -> str:
-        pass
+        return '\n'.join(str(v) for v in self.values)
 
     def __repr__(self) -> str:
-        pass
+        rows, cols = self.dim
+        return 'Matrix({} X {})'.format(rows, cols)
 
     def __add__(self, other):
-        pass
+        dim1 = self.dim
+        dim2 = self.dim
+        if dim1 == dim2:
+            values1 = self.values
+            values2 = other.values
+            for i, v in enumerate(values1):
+                for j, _ in enumerate(v):
+                    values1[i][j] += values2[i][j]
+        else:
+            raise ValueError('Dimension error')
+        return self
 
     def __sub__(self, other):
-        pass
+        dim1 = self.dim
+        dim2 = self.dim
+        if dim1 == dim2:
+            values1 = self.values
+            values2 = other.values
+            for i, v in enumerate(values1):
+                for j, _ in enumerate(v):
+                    values1[i][j] -= values2[i][j]
+        else:
+            raise ValueError('Dimension error')
+        return self
 
     def __mult__(self, other):
         pass
 
-    def __ne__(self, o: object) -> bool:
-        pass
+    def __neg__(self):
+        v = self.values
+        for i, v in enumerate(v):
+            for j, _ in enumerate(v):
+                v[i][j] -v[i][j]
 
     def __eq__(self, other):
-        pass
-
-    def inverse(self) -> int:
         pass
 
     def dim(self) -> list:
---
+++ test.py
@@ -0,0 +1,5 @@
+from matrix import Matrix
+
+m = Matrix([[1,2], [3, 4]])
+n = Matrix([[-1,2], [3, -4]])
+print(m - n)
