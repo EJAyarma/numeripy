53d78a880b4338b0b81b99e36f704fe7e89e9644
Ayarmz <johnayarmahe@gmail.com>
2021-05-07 18:40:31 +0000

    added a few methods and some documentation

--- matrix.py
+++ matrix.py
@@ -1,5 +1,6 @@
 """
-This class models a matrix
+This class models a matrix. It contains methods for common
+matrix operations and other methods for generating matrices.
 """
 
 from collections import namedtuple
@@ -13,16 +14,82 @@ class Matrix():
         self.values = values
         self.dim = Dimension(len(values), len(values[0]))
 
+    def __str__(self):
+        return '\n'.join(str(v) for v in self.values)
+
+    def __repr__(self):
+        rows, cols = self.dim
+        return 'Matrix({} X {})'.format(rows, cols)
+
+    def __add__(self, other):
+        """Overloading of addition operator on two matrices
+        i.e. m1 + m2 where both m1 and m2 are Matrix objects"""
+        dim1 = self.dim
+        dim2 = self.dim
+        if dim1 == dim2:
+            values1 = self.values
+            values2 = other.values
+            for i, v in enumerate(values1):
+                for j, _ in enumerate(v):
+                    values1[i][j] += values2[i][j]
+        else:
+            raise ValueError('Dimension error')
+        return self
+
+    def __sub__(self, other):
+        """Overloading of subtraction operator on two matrices
+        i.e. m1 - m2 where both m1 and m2 are Matrix objects"""
+        return self.__add__(-other)
+
+    def __mul__(self, other):
+        """Overloading of multiplication operator on two matrices
+        i.e. m1 * m2 where both m1 and m2 are Matrix objects"""
+        rows1, cols1  = self.dim
+        rows2, cols2 = other.dim
+        if cols1 == rows2:
+            new_m = self.from_zeros(rows1, cols2)
+            s_v = self.values
+            o_v = other.values
+            for j in range(rows1):
+                for k in range(cols2):
+                    a = [s_v[j][l] * o_v[l][k] for l in range(cols1)]
+                    new_m.values[j][k] = sum(a)
+            return new_m
+        else:
+            raise ValueError('Dimension error')
+
+    def __neg__(self):
+        """Overloading of negation operator on two matrices
+        i.e. -m where m is a Matrix object"""
+        val = self.values
+        for i, v in enumerate(val):
+            for j, _ in enumerate(v):
+                val[i][j] = -val[i][j]
+        self.values = val
+        return self
+
+    def __eq__(self, other):
+        """Overloading of equality conditional operator on two matrices
+        i.e. m1 == m2 where both m1 and m2 are Matrix objects"""
+        return self.values == other.values
+
     @classmethod
     def from_string(cls, s):
         """
-        Create a matrix from string
+        Constructor to create a matrix from string
         """
-        values = [r.split() for r in s.split('; ')]
-        return cls(values)
+        v = []
+        v_str = (r.split() for r in s.split('; '))
+        for el in v_str:
+            v_num = [int(n) for n in el]
+            v.append(v_num)
+
+        return cls(v)
 
     @classmethod
     def from_zeros(cls, r, c=None):
+        """Constructor to create an r by c or r by r
+        matrix with all entries 0"""
         if c == None:
             values = [[0]*r for _ in range(r)]
             return cls(values)
@@ -32,12 +99,13 @@ class Matrix():
 
     @classmethod
     def unit_matrix(cls, n):
-        """Create an n by n unit matrix"""
+        """Constructor to create an n by n unit matrix"""
+        # Create n by n matrix with all entries 0
         zeros_matrix = cls.from_zeros(n)
         values = zeros_matrix.values
         for i in range(n):
             row = values[i]
-            row[i] = 1
+            row[i] = 1 # Make all diagonal entries 1
         return zeros_matrix
 
     @staticmethod
@@ -49,6 +117,7 @@ class Matrix():
                 el = m.values[i]
                 for j in range(len(el)):
                     if j < i:
+                        # make entries below diagonal, 0
                         m.values[i][j] = 0
         else:
             raise ValueError('Dimension error')
@@ -63,8 +132,10 @@ class Matrix():
                 el = m.values[i]
                 for j in range(len(el)):
                     if j < i:
+                        # make entries below diagonal, 0
                         m.values[i][j] = 0
                     elif j == i:
+                        # make entries of diagonal, 1
                         m.values[i][j] = 1
         else:
             raise ValueError('Dimension error')
@@ -79,6 +150,7 @@ class Matrix():
                 el = m.values[i]
                 for j in range(len(el)):
                     if j > i:
+                        # make entries above diagonal, 0
                         m.values[i][j] = 0
         else:
             raise ValueError('Dimension error')
@@ -93,8 +165,10 @@ class Matrix():
                 el = m.values[i]
                 for j in range(len(el)):
                     if j > i:
+                        # make entries above diagonal, 0
                         m.values[i][j] = 0
                     elif j == i:
+                        # make diagonal entries 1
                         m.values[i][j] = 1
         else:
             raise ValueError('Dimension error')
@@ -102,69 +176,85 @@ class Matrix():
 
     def trns(self):
         """return the transpose of a matrix"""
-        cols, rows = self.dim
-        t_m = self.from_zeros(rows, cols)
+        cols, rows = self.dim # Swaping of rows and columns
+        t_m = self.from_zeros(rows, cols) # new matrix of zeros
+        #the new matrix has the dimension of the transpose
+
+        # generate entries of the transpose
         values = (self.values[j][i] for i, v in enumerate(t_m.values)
                   for j, _ in enumerate(v))
+        # populate entries into the new matrix
         for k in range(rows):
             for l in range(cols):
                 t_m.values[k][l] = next(values)
         return t_m
 
     def det(self):
-        pass
+        """Wrapper around _determinant() method"""
+        if self.is_square_matrix():
+            return self._determinant(self.values)
+        else:
+            raise ValueError('Dimension error')
+
+    def _determinant(self, values):
+        """Actual implementation of deterinant which uses recursion.
+        This is a naive implementation since at the moment it can only 
+        work for up to 3 by 3 matrix. Anything above 3 by 3 exhausts Python's
+        Maximum recursion depth"""
+        if len(values[0]) == 1: # deterinant of a 1 by 1 matrix
+            return values[0][0]
+        elif len(values[0]) == 2:  # determinant of a 2 by 2 matrix
+            d = (values[0][0] * values[1][1]) - (values[1][0] * values[0][1])
+            return d
+        elif len(values[0]) >= 3: # determinant of a n by n matrix where n >= 3
+            row1 = values[0]
+            d = 0
+            for i, factor in enumerate(row1):
+                sign = (-1)**i
+                # index is supplied to the _minor_matrix() method as starting from 1
+                minor = self._determinant(self._minor_matrix(1, i + 1)) 
+                d += factor * sign * minor
+            return  d
+
+
+    def _minor_matrix(self, a, b):
+        """Return the minor of a particular 
+        element in a square matrix at entry a,b
+        The indices provided start from 1 but we do computations
+        using lists which are zero indexed"""
+        
+        # Hence indices are changed to begin from 0
+        a -= 1 
+        b -= 1
+
+        l = [] # list to store minor matrix entries
+        for i, v in enumerate(self.values):
+            m = [] # temporary row
+            for j, w in enumerate(v):
+                if not (i == a or j == b):
+                    # append entries which are not in the row or column
+                    # specified by a or b respectively
+                    m.append(w)
+            if len(m) != 0:
+                l.append(m)
+        return l
 
     def inv(self):
         """Return the inverse of a matrix"""
         pass
 
-    def __str__(self) -> str:
-        return '\n'.join(str(v) for v in self.values)
-
-    def __repr__(self) -> str:
+    def is_square_matrix(self):
+        """Determine whether a matrix is square"""
         rows, cols = self.dim
-        return 'Matrix({} X {})'.format(rows, cols)
+        return rows == cols
 
-    def __add__(self, other):
-        dim1 = self.dim
-        dim2 = self.dim
-        if dim1 == dim2:
-            values1 = self.values
-            values2 = other.values
-            for i, v in enumerate(values1):
-                for j, _ in enumerate(v):
-                    values1[i][j] += values2[i][j]
-        else:
-            raise ValueError('Dimension error')
-        return self
+    def is_symmetric(self):
+        """"Determine whether a matrix is symmetric"""
+        return self.trns() == self
 
-    def __sub__(self, other):
-        dim1 = self.dim
-        dim2 = self.dim
-        if dim1 == dim2:
-            values1 = self.values
-            values2 = other.values
-            for i, v in enumerate(values1):
-                for j, _ in enumerate(v):
-                    values1[i][j] -= values2[i][j]
-        else:
-            raise ValueError('Dimension error')
-        return self
-
-    def __mult__(self, other):
-        pass
-
-    def __neg__(self):
-        v = self.values
-        for i, v in enumerate(v):
-            for j, _ in enumerate(v):
-                v[i][j] -v[i][j]
-
-    def __eq__(self, other):
-        pass
-
-    def dim(self) -> list:
-        pass
+    def is_symmetric(self):
+        """"Determine whether a matrix is skew-symmetric"""
+        return self.trns() == -self
 
     def is_utm(self):
         pass
@@ -176,7 +266,4 @@ class Matrix():
         pass
 
     def is_unit_ltm(self):
-        pass
-
-    def is_symmetric(self):
         pass
--- test.py
+++ test.py
@@ -1,5 +1,39 @@
+import unittest
 from matrix import Matrix
 
-m = Matrix([[1,2], [3, 4]])
-n = Matrix([[-1,2], [3, -4]])
-print(m - n)
+
+class TestMatrix(unittest.TestCase):
+
+    def setUp(self):
+        self.m1 = Matrix.from_string('1 2; 3 4')
+        self.m2 = Matrix.from_string('1 2; 3 4')
+        self.m3 = Matrix.from_string('5; 6')
+        self.m4 = Matrix.from_string('1 2 3; 4 5 6; 7 8 9')
+        self.m5 = Matrix.from_string('1 2 3 0; 6 5 45 0; 7 8 9 0; 0 0 0 1')
+
+    def test_matrix_from_string(self):
+        self.assertListEqual(self.m1.values, [[1, 2], [3, 4]])
+
+    def test_matrix_add(self):
+        m = self.m1 - self.m2
+        self.assertListEqual(m.values, [[0, 0], [0, 0]])
+
+    def test_matrix_multiply(self):
+        m = self.m1 * self.m3
+        self.assertListEqual(m.values, [[17], [39]])
+
+    def test_equalityy(self):
+        b = self.m1 == self.m2
+        self.assertTrue(b)
+
+    def test_minor_matrix(self):
+        minor_matrix = self.m4._minor_matrix(2, 3)
+        self.assertListEqual(minor_matrix, [[1, 2], [7, 8]])
+
+    def test_det(self):
+        d = self.m5.det()
+        self.assertEqual(d, 246)
+
+
+if __name__ == '__main__':
+    unittest.main()
